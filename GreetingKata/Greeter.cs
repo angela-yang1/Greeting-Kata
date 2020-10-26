2331d884baeba9c87d47cc18421269690ba0ad64
Angela <angela.yang@myob.com>
2020-10-27 11:37:24 +1300

    completed more steps

--- GreetingKata/Greeter.cs
+++ GreetingKata/Greeter.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 
 namespace GreetingKata
 {
@@ -6,20 +7,36 @@ namespace GreetingKata
     {
         public string Greet(string name)
         {
-            var isNameEmpty = string.IsNullOrEmpty(name);
-            var isNameInCaps = name == name?.ToUpper();
-
-            var standardGreetingMessage = $"Hello, {(isNameEmpty ? "my friend" : name)}.";
-            var upperCaseGreetingMessage = $"HELLO, {name}!";
+            if (string.IsNullOrEmpty(name))
+                return "Hello, my friend.";
             
-            return isNameInCaps ?  upperCaseGreetingMessage : standardGreetingMessage;
+            var isNameInCaps = name == name.ToUpper();
+            var hello = isNameInCaps ? "HELLO" : "Hello";
+            var endingPunctuation = isNameInCaps ? "!" : ".";
+            return $"{hello}, {name}{endingPunctuation}";
         }
-
+        
         public string Greet(IEnumerable<string> names)
         {
-            var greetingNames = string.Join(" and ", names);
+            // var greetingNames = string.Join(" and ", names);
+            var namesList = names.ToList();
+            var normalCaseNameList = namesList.Where(name => name != name.ToUpper()).ToList();
+            var upperCaseNameList = namesList.Where(name => name == name.ToUpper()).ToList();
+            
+            var normalCaseNameCount = normalCaseNameList.Count();
+            var normalCaseNameList1 = normalCaseNameList.GetRange(0, normalCaseNameCount - 1);
+            var normalCaseNamesCommaDelimited = DelimitWithComma(normalCaseNameList1);
+            
+            var upperCaseGreetingMessage = "";
+            if (upperCaseNameList.Count > 0)
+                upperCaseGreetingMessage = $" AND HELLO {upperCaseNameList[0]}!";
+            
+            return $"Hello, {normalCaseNamesCommaDelimited} and {normalCaseNameList[normalCaseNameCount - 1]}.{upperCaseGreetingMessage}";
+        }
 
-            return $"Hello, {greetingNames}.";
+        private string DelimitWithComma(List<string> names)
+        {
+            return string.Join(", ", names);
         }
     }
 }
